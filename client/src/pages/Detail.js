585cf24521016e958d254795f2fd71e1f653ac88
Sonmi Kos <sonmi.kos@gmail.com>
2019-10-05 22:46:20 -0500

    killed jumbotron

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Container } from "@material-ui/core";
 
@@ -22,11 +21,11 @@ class Detail extends Component {
       <Container fluid>
         <Row>
           <Col size="md-12">
-            <Jumbotron>
+            <div>
               <h1>
                 {this.state.book.title} by {this.state.book.author}
               </h1>
-            </Jumbotron>
+            </div>
           </Col>
         </Row>
         <Row>
